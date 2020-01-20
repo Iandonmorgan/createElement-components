093929cb2c4fe24ac470813c3b948548805cd1a8
Landon Morgan <morganlandon@gmail.com>
2020-01-20 14:58:01 -0600

    completed document fragment practice

--- main.js
+++ main.js
@@ -11,28 +11,31 @@ list.appendChild(listItem)
 
 console.log(list)
 
+const fragment = document.createDocumentFragment();
+
 const section1 = document.createElement('section');
 section1.className = "message";
 section1.textContent = "A: Hey, how are you?";
+fragment.appendChild(section1);
 
 const section2 = document.createElement('section');
 section2.className = "message";
 section2.textContent = "B: Doing well, you?";
+fragment.appendChild(section2);
 
 const section3 = document.createElement('section');
 section3.className = "message";
 section3.textContent = "A: Also well. I'm looking forward to tomorrow.";
+fragment.appendChild(section3);
 
 const section4 = document.createElement('section');
 section4.className = "message";
 section4.textContent = "B: What's tomorrow?";
+fragment.appendChild(section4);
 
 const section5 = document.createElement('section');
 section5.className = "message";
 section5.textContent = "A: The day after today.";
+fragment.appendChild(section5);
 
-document.querySelector("#messages").appendChild(section1);
-document.querySelector("#messages").appendChild(section2);
-document.querySelector("#messages").appendChild(section3);
-document.querySelector("#messages").appendChild(section4);
-document.querySelector("#messages").appendChild(section5);
+document.querySelector("#messages").appendChild(fragment);
